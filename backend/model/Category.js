c904b215a4f49db754e2f20a49b953f55cf26a12
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-08-15 13:02:39 +0530

    Data Modelling is done

---
+++ backend/model/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      default: "Uncategorized",
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["income", "expense"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+mongoose.exports = mongoose.model("Category", categorySchema);
