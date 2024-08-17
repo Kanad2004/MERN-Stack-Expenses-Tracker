e7e9dc5be321f3191766b475ed26a6b636d172eb
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-08-17 14:31:19 +0530

    Configured react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store/store.js";
-import {Provider} from 'react-redux'
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+//!instance of react query
+const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen = {false}/>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
