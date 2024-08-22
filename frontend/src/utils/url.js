24a492e5bbe59259970e206ecc81356ad7704801
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-08-22 19:25:59 +0530

    Final Commit

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
+
 export const BASE_URL = "http://localhost:8000/api/v1";
-=======
-export const BASE_URL = 'https://mern-stack-expenses-tracker-api.vercel.app/'
->>>>>>> 48d67a5a6d8495902a8527d714fb82aac15db210
